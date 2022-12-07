2b5d111fee084cd6e14bd28f0cb73b7a88542b23
ngduythao2805 <ngduythao2805@gmail.com>
2022-12-07 14:52:29 +0700

    feat: initial project

---
+++ src/appConfig.ts
@@ -0,0 +1,11 @@
+export const app: { [key: string]: string } = {
+  name: 'Zero-knowledge proof Link', // web app name
+  title: 'Zero-knowledge proof Link', // website title
+  description: 'Zero-knowledge proof Link', // description of website
+  keywords: 'web3, dapp, ethereum, ethers, wagmi, rainbowkit, nextjs, react, tailwindcss, sass', // keywords for SEO
+  url: '', // website url
+  image: '/icon-512x512.png', // image url for SEO
+  favicon: '/favicon.ico', // favicon url
+  themeColor: '#FFFFFF', // theme color
+  themeColorDark: '#000000', // theme color dark
+}
